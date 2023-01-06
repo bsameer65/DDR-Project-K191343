4394fd9b8a5bfef9a8fcbed5e4393119c686bcf5
unknown <sameerk6500@gmail.com>
2023-01-06 17:50:40 +0500

    Pattern implementation added interface

---
+++ MailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ACM_System
+{
+    public interface IMailService
+    {
+         void sendmail();
+    }
+    
+
+}
