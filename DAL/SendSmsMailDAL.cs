a8c7dc164800b94bf5dab4defdab7ef7cfb0347b
unknown <sameerk6500@gmail.com>
2023-01-06 22:16:16 +0500

    added function to select distinct city

---
+++ DAL/SendSmsMailDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACM_System.DAL
+{
+    class SendSmsMailDAL
+    {
+        SqlConnection connection = new SqlConnection(Connection.connectionString);
+
+        //Select Distinct City
+        public DataTable UniqueCitySelection()
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                string sql = "SELECT DISTINCT city FROM Client";
+                SqlCommand command = new SqlCommand(sql, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                connection.Open();
+                adapter.Fill(table);
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
+            return table;
+        }
+
+
+        
+    }
+}
